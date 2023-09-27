7fdfd37c3486dfe31d52b27ba36f9bbf7781fdb9
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-27 23:09:39 +0530

    Added Datatypes Folder

---
+++ Datatypes/Datatypes.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int a = 10;
+
+    long b = 2324;
+
+    long long c = 31231058834;
+
+    float d = 865.34368;
+
+    double e = 74312323.32596;
+
+    char f = 'f';
+
+    string g = "eisgod";
+
+    string str;
+    
+    getline(cin, str);
+
+    return 0;
+}
