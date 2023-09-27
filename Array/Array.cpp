40ee11de57b334f01c9d6e9a8a6384a8183aba18
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-09-27 23:09:22 +0530

    Added Array Folder

---
+++ Array/Array.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int arr[3];
+
+    cin >> arr[0] >> arr[1] >> arr[2];
+
+    cout << arr[1] << endl;
+
+    arr[2] = arr[2] + 10;
+
+    cout << arr[2] << endl;
+
+    int nums[] = {10, 7, 21};
+
+    int twoDarr[2][3] = {{1,2,3}, {4,5,6}};
+
+    return 0;
+}
