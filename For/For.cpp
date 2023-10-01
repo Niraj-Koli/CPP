7888b3cb17f42a630edb186b5024e423f4ebbe01
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-10-01 22:31:45 +0530

    Added For File

---
+++ For/For.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    for (int i = 1; i <= n; i++) {
+        cout << "Kaizer" << endl;
+    }
+
+    for (int j = n; j > 0; j--) {
+        cout << "Williams" << endl;
+    }
+
+    return 0;
+}
