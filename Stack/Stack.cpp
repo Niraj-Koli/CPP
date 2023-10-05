5cc3a7511e95d766c816663dffd0f91852f14f08
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-10-05 20:20:16 +0530

    Added Stack File

---
+++ Stack/Stack.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    stack<int> st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+
+    st.pop();
+
+    st.top();
+
+    st.size();
+
+    st.empty();
+
+    stack<int>st1, st2;
+
+    st1.swap(st2);
+
+    return 0;
+}
