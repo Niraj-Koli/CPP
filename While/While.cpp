96ede9a232c12d9d8843b2a67db0ef83de5b41a2
Niraj Koli <113058131+NirajKoli@users.noreply.github.com>
2023-10-06 18:51:19 +0530

    Added While File'

---
+++ While/While.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    int n;
+
+    cin >> n;
+
+    int i = 1;
+
+    while (i <= n) {
+        cout << "Kaizer" << endl;
+
+        i++;
+    }
+
+    int j = n;
+
+    while (j > 0) {
+        cout << "Williams" << endl;
+
+        j--;
+    }
+
+    return 0;
+}
